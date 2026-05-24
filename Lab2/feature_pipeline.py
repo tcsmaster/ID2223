6d69276da203efd3ec64ce9f6ca60820ec9e79a0
tcsmaster <csanad.t@gmail.com>
2026-05-24 10:30:35 +0200

    update before ssd change

--- Lab2/feature_pipeline.py
+++ Lab2/feature_pipeline.py
@@ -1,9 +1,9 @@
 import asyncio
 import os
 
-import modal
 from dotenv import load_dotenv
 from modal import App, Image, Secret, Volume
+from transformers import WhisperFeatureExtractor, WhisperTokenizer
 
 load_dotenv()
 hf_token = os.environ['HF_TOKEN']
@@ -15,7 +15,7 @@ MINUTES = 60
 CPU_REQUEST = 2.0
 CPU_LIMIT = 4.0
 MEMORY_REQUEST=8192
-MEMORY_LIMIT=8192
+MEMORY_LIMIT=16384
 
 image = Image\
     .from_registry('python:3.12-slim-bookworm') \
@@ -29,9 +29,6 @@ secret = Secret.from_dotenv(__file__)
 
 app = App("Hungarian AS(M)R", image=image, volumes = {MODEL_FOLDER:model_volume, DATASET_PATH:data_volume}, secrets=[secret])
 """
-with data_volume.batch_upload() as batch:
-    batch.put_directory('./dataset', '/') # with using data_volume, / is already /dataset in the volume
-
 @app.function(timeout=20*MINUTES)
 def create_dataset():
     from datasets import Audio, load_dataset
@@ -41,43 +38,33 @@ def create_dataset():
 """
 
 @app.function(timeout=10*MINUTES)
-def fre_load_model():
+def load_model():
     from huggingface_hub import snapshot_download
     snapshot_download(MODEL_NAME, ignore_patterns = ['*.h5', '*.bin'])
 
-@app.cls(timeout = 5*MINUTES,max_containers=3,)
-class Extractor:
-
-    @modal.enter()
-    def load(self):
-        from transformers import WhisperFeatureExtractor, WhisperTokenizer
-        self.extractor =  WhisperFeatureExtractor.from_pretrained(MODEL_NAME, hf_token=hf_token,  cache_dir=MODEL_FOLDER)
-        self.tokenizer =  WhisperTokenizer.from_pretrained(MODEL_NAME, hf_token=hf_token,  cache_dir=MODEL_FOLDER)
-        
-    @modal.batched(wait_ms = 60000, max_batch_size=200)
-    def process(self, batch):
-        feat = self.extractor([el['audio'].get_all_samples().data.squeeze().numpy for el in batch], sampling_rate=16_000, padding=True)
-        feat = list(feat.input_features)
-        labels = self.tokenizer(batch['text']).input_ids
-        return [dict(input_features=feats, labels=token) for feats, token in zip(feat, labels)]
+def process(batch:dict, extractor, tokenizer):
+    feat =extractor([el.get_all_samples().data.squeeze().numpy() for el in batch['audio']], sampling_rate=16_000, padding="max_length", return_attention_mask=True)
+    batch['input_features'] = list(feat.input_features)
+    batch['attention_mask'] = list(feat.attention_mask)
+    batch['labels'] = tokenizer(batch['sentence']).input_ids
+    return batch
 
 @app.function(
         timeout=60*MINUTES,
+        cpu = (CPU_REQUEST, CPU_LIMIT),
+        memory=(MEMORY_REQUEST, MEMORY_LIMIT),
+        max_containers=3,
 )
 async def transcribe_dataset(split:str):
-    from datasets import load_dataset
-    ext = Extractor()
-    ds = load_dataset('hf-internal-testing/librispeech_asr_dummy', 'clean', split='validation')
-    result = []
-    async for res in ext.process.map.aio(ds):
-        result.extend(res)
-    print(len(result))
-    # new_ds = Dataset.from_list(result)
-    # new_ds.save_to_disk(f'/dataset/processed_clips/{split}')
-    # data_volume.commit()
+    from datasets import load_from_disk
+    extractor =  WhisperFeatureExtractor.from_pretrained(MODEL_NAME, hf_token=hf_token,  cache_dir=MODEL_FOLDER)
+    tokenizer =  WhisperTokenizer.from_pretrained(MODEL_NAME, hf_token=hf_token,  cache_dir=MODEL_FOLDER)
+    ds = load_from_disk(f"/dataset/{split}")
+    ds = ds.map(process, fn_kwargs = dict(extractor=extractor, tokenizer=tokenizer), batch_size=200, batched=True, remove_columns = ds.column_names)
+    ds.save_to_disk(f'/dataset/processed_{split}', max_shard_size='1GB', num_proc = os.cpu_count() - 1)
+    await data_volume.commit.aio()
 
 @app.local_entrypoint()
 async def main() -> None:
-    # split =['train', 'dev', 'test']
-    split = ['we']
+    split =['train', 'dev', 'test']
     await asyncio.gather(*[transcribe_dataset.remote.aio(sp) for sp in split])
--- Lab2/training_pipeline.py
+++ Lab2/training_pipeline.py
@@ -1,51 +1,77 @@
+import functools
+import os
 from dataclasses import dataclass
-from typing import Any, Dict, List, Union
+from pathlib import Path
+from typing import Any, Dict, List, Optional, Union
 
+import datasets
 import evaluate
 import torch
 import transformers
-from datasets import Dataset, DatasetDict
-from modal import App, Image, Secret, Volume
-from transformers import (
-    Seq2SeqTrainer,
-    Seq2SeqTrainingArguments,
-    WhisperForConditionalGeneration,
-    WhisperProcessor,
-)
+from datasets import Dataset
+from modal import App, Image, Retries, Secret, Volume
 
+MINUTES = 60
+HOURS = 60*MINUTES
 MODEL_FOLDER = '/model'
-MODEL_NAME = "openai/whisper-small"
 DATASET_PATH = '/dataset'
+OUTPUT_DIR = Path( '/output' )
+EXPERIMENT_PATH = OUTPUT_DIR / 'experiments'
+retries = Retries(initial_delay=0.0, max_retries=10)
 
 image = Image\
-    .from_registry('python:3.12-slim-bookworm') \
+    .debian_slim(python_version='3.12') \
     .apt_install(["ffmpeg"])\
     .uv_sync() \
-    .env({'HF_XET_HIGH_PERFORMANCE':'1', 'HF_HUB_CACHE':MODEL_FOLDER}) \
 
-volume = Volume.from_name('processed_data', create_if_missing=True)
+data_volume = Volume.from_name('processed_data', create_if_missing=True)
+model_volume = Volume.from_name('model_volume', create_if_missing=True)
+output_volume = Volume.from_name('output_volume', create_if_missing=True)
 secret = Secret.from_dotenv(__file__)
-DATASET_PATH = Path("./datasets")
-app =App("Hungarian AS(M)R", image=image, volumes = {DATASET_PATH:volume}, secrets=[secret])
+app =App("Hungarian AS(M)R", image=image, volumes = {MODEL_FOLDER:model_volume,DATASET_PATH:data_volume, OUTPUT_DIR:output_volume}, secrets=[secret])
+#TODO: wandb integration, hyperparameter tuning on dev dataset
+
+@dataclass
+class Config:
+    """Training configuration."""
+
+    model_output_name: str = "whisper-fine-tune"  # Name used for saving and loading
+    wandb_project_name:str = "Whisper"
+
+    # Model config
+    model_name: str = "openai/whisper-small"
+    dataset_path = Path(DATASET_PATH)
+    # Dataset config
+    dataset_split: str = "train"  # The test and val splits don't have category labels
+    max_duration_in_seconds: float = 20.0
+    min_duration_in_seconds: float = 0.0
+
+    # Training config
+    num_train_epochs: int = 5
+    warmup_steps: int = 400
+    max_steps: int = -1
+    batch_size: int = 64
+    learning_rate: float = 1e-5
+    eval_strategy: str = "epoch"
 
 def compute_metrics(pred:Dataset,metric:evaluate.Metric, tokenizer:transformers.AutoTokenizer):
-    pred_ids = pred.predicitons
+    pred_ids = pred.predictions
     label_ids = pred.label_ids
     label_ids[label_ids == -100] = tokenizer.pad_token_id
     pred_str = tokenizer.batch_decode(pred_ids, skip_special_tokens = True)
     label_str = tokenizer.batch_decode(label_ids, skip_special_tokens = True)
     wer = 100*metric.compute(predictions = pred_str, references = label_str)
-    return wer
+    return {'wer':wer}
 
 @dataclass
 class DataCollatorSpeechSeq2SeqWithPadding:
+    decoder_start_token_id: int
     processor: Any
     def __call__(self, features: List[Dict[str, Union[List[int], torch.Tensor]]]) -> Dict[str, torch.Tensor]:
         # split inputs and labels since they have to be of different lengths and need different padding methods,
         # first treat the audio inputs by simply returning torch tensors,
-        #TODO: define the processor from feature_pipeline.py here
         input_features = [{"input_features": feature["input_features"]} for feature in features]
-        batch = self.processor.feature_extractor.pad(input_features, return_tensors= "pt")
+        batch = self.processor.feature_extractor.pad(input_features, return_tensors= "pt") #TODO: is this necessary? the preprocess pads to max_length
         # get the tokenized label sequences,
         label_features = [{"input_ids": feature["labels"]} for feature in features]
         # pad the labels to max length,
@@ -59,47 +85,133 @@ class DataCollatorSpeechSeq2SeqWithPadding:
         batch["labels"] = labels
         return batch
 
-@app.function()
-def main():
-    common_voice= DatasetDict.load_from_disk("common_voice")
-    processor = WhisperProcessor('openai/whisper-small')
-    data_collator = DataCollatorSpeechSeq2SeqWithPadding(processor=processor)
-    metric = evaluate.load("wer")
-    model = WhisperForConditionalGeneration("openai/whisper-small")
-    model.config.forced_decoder_ids = None
-    model.config.suppress_tokens = []
-
-    training_args = Seq2SeqTrainingArguments(
-        num_train_epochs=1,
-        output_dir="./output_dir",
-        per_device_train_batch_size=16,
-        gradient_accumulation_steps=2,  # increase by 2x for every 2x decrease in batch size
-        learning_rate=1e-5,
-        warmup_steps=500,
-        max_steps=4000, # use less steps if needed
-        gradient_checkpointing=True,
-        fp16=True,
-        evaluation_strategy="steps",
-        per_device_eval_batch_size=4,
+@app.function(
+    image=image,
+    gpu="a10g",
+    timeout=3 * HOURS,
+    max_containers=1,
+    retries = retries,
+)
+def train(
+        experiment:str
+):
+    """Loads data and trains the model."""
+    config = Config
+    training_args = transformers.Seq2SeqTrainingArguments(
+        output_dir= EXPERIMENT_PATH / experiment,
+        num_train_epochs=config.num_train_epochs,
+        save_strategy="epoch",
+        per_device_train_batch_size=config.batch_size,
+        per_device_eval_batch_size=config.batch_size,
+        logging_steps=5,
+        learning_rate=config.learning_rate,
+        lr_scheduler_type='cosine',
+        warmup_steps=config.warmup_steps,
+        torch_compile=True,
+        max_steps=config.max_steps,
+        eval_strategy=config.eval_strategy,
+        bf16=True,
+        report_to = "wandb",
+        run_name = config.wandb_project_name,
         predict_with_generate=True,
-        generation_max_length=225,
-        eval_steps=500,
-        logging_steps=25,
-        report_to=None,
-        load_best_model_at_end=True,
-        metric_for_best_model="wer",
-        greater_is_better=False,
-        push_to_hub=True,
+        generation_max_length=40,
+        generation_num_beams=1,
     )
-    trainer = Seq2SeqTrainer(
-        args = training_args,
-        model = model,
-        train_dataset=common_voice['train'],
-        eval_dataset=common_voice['test'],
+
+    os.environ["WANDB_PROJECT"] = config.wandb_project_name
+    os.environ["WANDB_WATCH"] = "false"
+    os.environ["WANDB_API_KEY"] = os.environ["WANDB_API_KEY"]
+
+    print(f"Loading model: {config.model_name}")
+    feature_extractor = transformers.WhisperFeatureExtractor.from_pretrained(
+        config.model_name, cache_dir = MODEL_FOLDER
+    )
+    tokenizer = transformers.WhisperTokenizer.from_pretrained(
+        config.model_name, cache_dir = MODEL_FOLDER
+    )
+    model = transformers.WhisperForConditionalGeneration.from_pretrained(
+        config.model_name, cache_dir = MODEL_FOLDER
+    )
+    ds1 = datasets.load_from_disk(config.dataset_path / "processed_train")
+    ds2 = datasets.load_from_disk(config.dataset_path / "processed_dev")
+    ds3 = datasets.load_from_disk(config.dataset_path / "processed_test")
+    dataset = datasets.concatenate_datasets([ds1, ds2])
+
+    # Create a processor that combines the feature extractor and tokenizer
+    processor = transformers.WhisperProcessor(
+        feature_extractor=feature_extractor,
+        tokenizer=tokenizer,
+    )
+
+    # Custom data collator handles batching of variable-length audio sequences
+    data_collator = DataCollatorSpeechSeq2SeqWithPadding(
+        processor=processor,
+        decoder_start_token_id=model.config.decoder_start_token_id,
+    )
+
+    # Set up the Hugging Face trainer with all of our components
+    metric = evaluate.load("wer")
+
+    trainer = transformers.Seq2SeqTrainer(
+        model=model,
+        args=training_args,
+        train_dataset=dataset, 
+        eval_dataset=ds3,
+        processing_class=feature_extractor,
         data_collator=data_collator,
-        compute_metrics=compute_metrics,
-        tokenizer=processor.feature_extractor,
+        compute_metrics=functools.partial(
+            compute_metrics,
+            tokenizer=tokenizer,
+            metric=metric,
+        ),
     )
-    trainer.train()
-if __name__ == "__main__":
-    main()
+
+    print("Running evals before training to establish a baseline")
+    metrics = trainer.evaluate(
+        metric_key_prefix="baseline",
+        max_length=training_args.generation_max_length,
+        num_beams=training_args.generation_num_beams,
+    )
+    trainer.log_metrics("baseline", metrics)
+    trainer.save_metrics("baseline", metrics)
+
+    print(f"Starting training! Weights will be saved to '{training_args.output_dir}'")
+    train_result = trainer.train()
+
+    # Save the model weights, tokenizer, and feature extractor
+    trainer.save_model()
+    tokenizer.save_pretrained(training_args.output_dir)
+    feature_extractor.save_pretrained(training_args.output_dir)
+
+    # Log training metrics
+    metrics = train_result.metrics
+    metrics["train_samples"] = len(dataset)
+    trainer.log_metrics("train", metrics)
+    trainer.save_metrics("train", metrics)
+    trainer.save_state()
+
+    # Final evaluation to see how much we improved
+    print("Running final evals")
+    metrics = trainer.evaluate(
+        metric_key_prefix="test",
+        max_length=training_args.generation_max_length,
+        num_beams=training_args.generation_num_beams,
+    )
+    metrics["eval_samples"] = len(dataset["test"])
+
+    trainer.log_metrics("test", metrics)
+    trainer.save_metrics("test", metrics)
+    output_volume.commit()  # Ensure everything is saved to the Volume
+
+    print(f"\nTraining complete! Model saved to '{training_args.output_dir}'")
+
+@app.local_entrypoint()
+def main(experiment:Optional[str] = None):
+    """Run Whisper fine-tuning on Modal."""
+    if experiment is None:  
+        import uuid
+        experiment = uuid.uuid4().hex[:8]
+    print(f"Staritng experiment {experiment}")
+
+    train.spawn(experiment).get()
+
